8d97d03d9f9c282df0591284ed4fc3de4e07b1c0
Hugo Demenez <hugodemenez@Hugos-MacBook-Pro.local>
2023-12-01 15:19:49 +0100

    feat(Analytics): Adding vercel analytics
    In order to keep track of the traffic the website uses vercel analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   const meta = {
@@ -25,6 +26,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   )
