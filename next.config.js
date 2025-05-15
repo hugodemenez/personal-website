d1e8142bfea5be02f4a6846c4efad027840c971b
Hugo DEMENEZ <hdemenez@hotmail.fr>
2025-05-15 13:38:46 +0200

    feat: update Next.js configuration and add Substack feed component
    - Configure image domains for external sources in next.config.js
    - Add fast-xml-parser dependency for XML parsing
    - Introduce CustomSubstackFeed component in the index page to display newsletter updates
    - Remove yarn.lock file to streamline dependency management

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+module.exports = withNextra({
+  images: {
+    domains: ['substackcdn.com', 'substack-post-media.s3.amazonaws.com'],
+  },
+})
